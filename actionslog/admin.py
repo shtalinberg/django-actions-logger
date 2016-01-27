f39d23910687bebe0374c98ada3eb0b80bcf1998
Shtalinberg Oleksandr <O.Shtalinberg@gmail.com>
2016-01-27 18:40:16 +0200

    fix names

--- actionslog/admin.py
+++ actionslog/admin.py
@@ -7,7 +7,8 @@ from .models import LogAction
 @admin.register(LogAction)
 class LogActionAdmin(admin.ModelAdmin):
     """docstring for  """
-    list_display = ('id', 'created_at','remote_ip', 'user', 'action', 'object_repr')
+    list_display = ('id', 'created_at','remote_ip', 'user', 'action',
+        'object_repr', 'content_type', 'object_id' , 'object_extra_info', 'action_info')
     search_fields = ['user__email', 'remote_ip']
 
     date_hierarchy = 'created_at'
